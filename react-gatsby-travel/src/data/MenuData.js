0f9040877f4e85ac08b1d1b627765737bd5dcb4a
janf319 <janf@gmatc.matc.edu>
2023-04-28 21:55:14 -0500

    Set up Project

---
+++ react-gatsby-travel/src/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+    {title: "About", link: "/about"},
+    {title: "Trips", link: "/Trips"},
+    {title: "Careers", link: "/Career"},
+    {title: "Contact", link: "/Contact"},
+]
