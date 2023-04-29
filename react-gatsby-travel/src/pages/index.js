4a3c36b8762a786d8fd6eefab0d49b9400248c0a
janf319 <janf@gmatc.matc.edu>
2023-04-29 06:44:00 -0500

    Added Hero Component & Video

--- react-gatsby-travel/src/pages/index.js
+++ react-gatsby-travel/src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Hero from "../components/Hero"
 
 const IndexPage = () => (
   <Layout>
    <Seo title="Home" />
+   <Hero />
   </Layout>
 )
 
