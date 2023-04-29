5fa1a80f61a3b5af986c603da5e5a8adda6dca52
janf319 <janf@gmatc.matc.edu>
2023-04-29 06:27:38 -0500

    Added Button Component

--- react-gatsby-travel/src/components/Header.js
+++ react-gatsby-travel/src/components/Header.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import styled from 'styled-components'
 import { FaBars } from 'react-icons/fa'
 import { menuData } from "../data/MenuData"
+import { Button } from "./Button"
 
 const Header = () => {
   return (
@@ -17,7 +18,7 @@ const Header = () => {
         ))}
       </NavMenu>
       <NavBtn>
-      hey
+       <Button primary="true" round="true" to="/trips">Book a Flight</Button>
       </NavBtn>
     </Nav>
   )
